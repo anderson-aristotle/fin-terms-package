2b2e2170c7c710314f3427cf4aae717d00adf1bf
rex@billion <anderson.aristotle@gmail.com>
2023-05-17 22:27:39 -0400

    added more test data before NPM publish

--- creditScoreData.js
+++ creditScoreData.js
@@ -1,17 +1,70 @@
 const creditScoreData = [
   {
-    term: "Credit Score",
+    word: "Credit",
+    definition:
+      "The ability to borrow money or access goods and services with the expectation of repayment in the future.",
+    example: "She used her credit to purchase a new laptop.",
+    source: "https://www.example.com/credit",
+  },
+  {
+    word: "Revolving Credit",
+    definition:
+      "A type of credit that allows the borrower to repeatedly borrow up to a certain limit and make minimum payments or pay in full each month.",
+    example: "He has a credit card with a revolving credit line of $5,000.",
+    source: "https://www.example.com/revolving-credit",
+  },
+  {
+    word: "Charge Cards",
+    definition:
+      "A type of credit card that requires the cardholder to pay the balance in full each month.",
+    example: "She prefers using charge cards to avoid accumulating debt.",
+    source: "https://www.example.com/charge-cards",
+  },
+  {
+    word: "Installment Credit",
+    definition:
+      "A type of credit that involves borrowing a fixed amount and repaying it in equal installments over a specified period of time.",
+    example: "He took out an installment loan to buy a car.",
+    source: "https://www.example.com/installment-credit",
+  },
+  {
+    word: "Service Credit",
+    definition:
+      "Credit extended for services rendered, such as utility bills or cellphone plans.",
+    example:
+      "He pays his monthly utility bills on time to maintain good service credit.",
+    source: "https://www.example.com/service-credit",
+  },
+  {
+    word: "Credit Bureaus",
+    definition:
+      "Companies that collect and maintain credit information about individuals, including their borrowing and repayment history.",
+    example:
+      "The lender checked his credit report with the major credit bureaus.",
+    source: "https://www.example.com/credit-bureaus",
+  },
+  {
+    word: "Credit Report",
+    definition:
+      "A detailed record of an individual's credit history, including credit accounts, payment history, and public records.",
+    example: "She reviewed her credit report to check for any errors.",
+    source: "https://www.example.com/credit-report",
+  },
+  {
+    word: "Credit Score",
     definition:
       "A numerical representation of an individual's creditworthiness, typically ranging from 300 to 850.",
-    example: "John has a credit score of 720.",
+    example:
+      "His credit score is 750, which qualifies him for favorable loan terms.",
     source: "https://www.example.com/credit-score",
   },
   {
-    term: "Credit Report",
+    word: "Credit Utilization",
     definition:
-      "A detailed record of an individual's credit history, including credit accounts, payment history, and public records.",
-    example: "Sara reviewed her credit report to check for any errors.",
-    source: "https://www.example.com/credit-report",
+      "The percentage of available credit that is currently being used, often used as a factor in determining credit scores.",
+    example:
+      "She keeps her credit utilization below 30% to maintain a good credit score.",
+    source: "https://www.example.com/credit-utilization",
   },
 ];
 
